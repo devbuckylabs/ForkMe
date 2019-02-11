1f22b9117ddfc7bfebf753d8bc20f00f29d758e7
devbuckylabs <dev.buckylabs@gmail.com>
2019-02-11 19:47:53 +0530

    2 Commit

---
+++ java/EvenOdd.java
@@ -0,0 +1,17 @@
+package com.bucket;
+
+public class EvenOdd {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		int n = 3;
+		if (n % 2 == 0) {
+			System.out.println("Even");
+		} else {
+			System.out.println("Odd");
+		}
+
+	}
+
+}
